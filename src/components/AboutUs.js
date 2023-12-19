dfb0050bd3c298ed341c16d6c81bb97cec3b68fd
Rifqi <rifqihabib04@gmail.com>
2023-12-19 10:57:10 +0700

    third commit

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import rifqiImage from './img/rifqi.jpg'; 
+import dimas from './img/dimas.png';
+import haya from './img/haya.png';
+import jidan from './img/jidan.png';
+import lisa from './img/lisa.png';
+import bian from './img/bian.png';
 
 const AboutUs = () => {
   const developers = [
@@ -6,37 +12,37 @@ const AboutUs = () => {
       id: 1,
       name: 'Rifqi Habib Ur Rahman',
       position: 'Frontend Developer & Backend developer',
-      photo: 'https://via.placeholder.com/150', // Replace with actual photo URL
+      photo: rifqiImage, // Replace with actual photo URL
     },
     {
       id: 2,
       name: 'Luthfansya Haikal Ismulia',
       position: 'Frontend Developer',
-      photo: 'https://via.placeholder.com/150', // Replace with actual photo URL
+      photo: haya, // Replace with actual photo URL
     },
     {
         id: 3,
         name: 'Thelissa Levana Zheng',
         position: 'Frontend Developer',
-        photo: 'https://via.placeholder.com/150', // Replace with actual photo URL
+        photo: lisa, // Replace with actual photo URL
     },
     {
         id: 4,
         name: 'Dimas Bagus Nandito',
         position: 'UI Design',
-        photo: 'https://via.placeholder.com/150', // Replace with actual photo URL
+        photo: dimas, // Replace with actual photo URL
     },
     {
         id: 5,
         name: 'Muhammad Fabiansyah Abubakar',
         position: 'UI Design',
-        photo: 'https://via.placeholder.com/150', // Replace with actual photo URL
+        photo: bian, // Replace with actual photo URL
     },
     {
         id: 6,
         name: 'Muhammad Danish Zidane',
         position: 'UI & UX',
-        photo: 'https://via.placeholder.com/150', // Replace with actual photo URL
+        photo: jidan, // Replace with actual photo URL
       },
     // Add more developers similarly
   ];
@@ -44,25 +50,28 @@ const AboutUs = () => {
   return (
     
     <div className="about-us">
+      <div className="card1">
       <h2>Our Team</h2>
-      <div classname = "card">
-      <div className="developer-cards">
-        {developers.map((developer) => (
-          <div key={developer.id} className="developer-card">
-            <img src={developer.photo} alt={developer.name} />
-            <h3>{developer.name}</h3>
-            <p>{developer.position}</p>
-          </div>
-        ))}
+        <div className="developer-cards">
+          {developers.map((developer) => (
+            <div key={developer.id} className="developer-card">
+              <div className="photo-container">
+                <img src={developer.photo} alt={developer.name} />
+              </div>
+              <h3>{developer.name}</h3>
+              <p>{developer.position}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      </div>
-      
-      <div className="card">
+
+      <div className="card1">
       <div className="why-we-created">
         <h2>Why We Created This Website</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-          consectetur elit at nisi volutpat eleifend.
+        At our core, we strive to simplify and enhance everyday experiences. 
+        Our collective passion for seamless user interaction and accessible information led us to craft this weather checklist website. 
+        We envisioned a platform that transcends mere weather updates—a tool that not only provides forecasts but empowers users to plan and organize their activities efficiently. 
           {/* Add your explanation here */}
         </p>
       </div>
